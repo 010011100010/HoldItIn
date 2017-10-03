481ea5a5d3eb1a2baac8d5c11e3fd1e67ed7273a
010011100010 <ll3305@nyu.edu>
2017-10-03 10:58:03 -0400

    Unity is able to read data from arduino

---
+++ HoldItIn/Assets/Scripts/dataReading.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO.Ports;
+
+public class dataReading : MonoBehaviour {
+	SerialPort floraSP = new SerialPort ("COM9", 9600);
+	// Use this for initialization
+	void Start () {
+		floraSP.Open ();
+		floraSP.ReadTimeout = 1;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		try{
+			print (floraSP.ReadLine());
+		}
+		catch(System.Exception){
+		}
+	}
+}
