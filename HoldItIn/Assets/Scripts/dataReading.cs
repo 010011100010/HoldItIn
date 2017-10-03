f7414b04beb822924644050337aaf70898a94608
010011100010 <ll3305@nyu.edu>
2017-10-03 16:20:22 -0400

    Rectal pressure value is working

--- HoldItIn/Assets/Scripts/dataReading.cs
+++ HoldItIn/Assets/Scripts/dataReading.cs
@@ -6,31 +6,67 @@ using System.IO.Ports;
 
 public class dataReading : MonoBehaviour {
 	SerialPort floraSP = new SerialPort ("COM9", 9600);
-	SerialPort floraSP2 = new SerialPort ("COM9", 4800);
+	//SerialPort floraSP2 = new SerialPort ("COM9", 4800);
 
-	private Text switchText;
-	private Text bendText;
+	public Color blue;
+	public Color green;
+	public Color red;
+
+	public Text switchText;
+	public Text bendText;
+	private Text rectalPressureText;
+
+	private Camera cam;
+	private float bendSqueezePressure;
+	private float rectalPressureValue = 1000;
+	private float bendSqueezeValue;
+	public float leakSpeed = 2;
+	public float leakThreshold = 0;
+	public float overPressureThreshold = 2000;
+	public float colorLerpDuration = 3f;
+
+
 	// Use this for initialization
 	void Start () {
+
 		floraSP.Open ();
-		floraSP.ReadTimeout = 2;
+		floraSP.ReadTimeout = 1;
 
-		floraSP2.Open ();
-		floraSP2.ReadTimeout = 2;
+		//floraSP2.Open ();
+		//floraSP2.ReadTimeout = 2;
 
-		switchText = GameObject.Find ("PressureSwitch").GetComponent<Text> ();
-		bendText = GameObject.Find ("PressureBend").GetComponent<Text> ();
+		//switchText = GameObject.Find ("PressureSwitch").GetComponent<Text> ();
+		//bendText = GameObject.Find ("PressureBend").GetComponent<Text> ();
+		//switchText.text = "Pressure Switch Value: ";
+		//Debug.Log (switchText.text);
+		cam = GameObject.Find("Main Camera").GetComponent<Camera> ();
+		rectalPressureText = GameObject.Find ("RectalPressure").GetComponent<Text> ();
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		//print (Mathf.Round (Time.deltaTime));
+		bendSqueezePressure = float.Parse(floraSP.ReadLine ());
+		bendSqueezeValue = 950f - bendSqueezePressure;
+		rectalPressureValue = rectalPressureValue - Time.deltaTime*leakSpeed + bendSqueezeValue;
+
 		try{
-			print (floraSP.ReadLine());
+			//print (floraSP.ReadLine());
 		}
 		catch(System.Exception){
 		}
 
-		switchText.text = "Pressure Switch Value: " + floraSP.ReadLine ();
-		bendText.text = "Pressure Bend Value: " + floraSP2.ReadLine ();
+		switchText.text = "Pressure Switch Value: " + bendSqueezeValue;
+		rectalPressureText.text = "Rectal Pressure: " + (Mathf.Round(rectalPressureValue));
+		//bendText.text = "Pressure Bend Value: "; //+ floraSP2.ReadLine ();
+		float t = Mathf.PingPong(Time.time, colorLerpDuration)/colorLerpDuration;
+		if (rectalPressureValue < leakThreshold) {
+			cam.backgroundColor = Color.Lerp (cam.backgroundColor, green, t);
+		} else if (rectalPressureValue > overPressureThreshold) {
+			cam.backgroundColor = Color.Lerp (cam.backgroundColor, red, t);
+		} else {
+			cam.backgroundColor = Color.Lerp (cam.backgroundColor, blue, t);
+		}
 	}
 }
