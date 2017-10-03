7cb95b4ab9c24ddbf5ec8e908a8d609101e5cba9
010011100010 <ll3305@nyu.edu>
2017-10-03 12:34:01 -0400

    Unity Serial read test

--- HoldItIn/Assets/Scripts/dataReading.cs
+++ HoldItIn/Assets/Scripts/dataReading.cs
@@ -1,14 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using System.IO.Ports;
 
 public class dataReading : MonoBehaviour {
 	SerialPort floraSP = new SerialPort ("COM9", 9600);
+	SerialPort floraSP2 = new SerialPort ("COM9", 4800);
+
+	private Text switchText;
+	private Text bendText;
 	// Use this for initialization
 	void Start () {
 		floraSP.Open ();
-		floraSP.ReadTimeout = 1;
+		floraSP.ReadTimeout = 2;
+
+		floraSP2.Open ();
+		floraSP2.ReadTimeout = 2;
+
+		switchText = GameObject.Find ("PressureSwitch").GetComponent<Text> ();
+		bendText = GameObject.Find ("PressureBend").GetComponent<Text> ();
 	}
 	
 	// Update is called once per frame
@@ -18,5 +29,8 @@ public class dataReading : MonoBehaviour {
 		}
 		catch(System.Exception){
 		}
+
+		switchText.text = "Pressure Switch Value: " + floraSP.ReadLine ();
+		bendText.text = "Pressure Bend Value: " + floraSP2.ReadLine ();
 	}
 }
